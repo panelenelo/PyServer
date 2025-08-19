0ca7ac43f965f6f2f8b9841a627abf24e02980aa
panelenelo <panelenelo@gmail.com>
2025-08-19 17:08:30 -0300

    study 2025/08/19

---
+++ Task.py
@@ -0,0 +1,23 @@
+#Task class
+
+class Task:
+    def __init__(self, id: int,title: str, description: str, is_complete: bool):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.is_complete = is_complete
+
+    def switch_complete():
+        is_complete = not(is_complete)
+
+    def get_Title(self):
+        return self.title
+
+    def get_id(self):
+        return self.id
+    
+    def get_description(self):
+        return self.description
+    
+    def get_completion(self):
+        return self.is_complete
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from Task import Task
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+
+@app.get("/tasks")
+async def read_tasks():
+    return {"task1": "task1"}
+
+@app.get("/tasks/{task_id}")
+async def read_task_item(task_id):
+    return{"task": task_id}
+
+@app.post("/tasks")
+async def post_task(task):
+    return{"Task created"}
+
+@app.put("/tasks/{task_id}")
+async def put_task(task):
+    return{"Task updated"}
+
+@app.delete("/tasks/{task_id}")
+async def delete_task(task_id):
+    return("Task deleted")
 
