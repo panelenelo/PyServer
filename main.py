11ef437cc62bfe5b8544b71d6fe0ee5593d8d7a2
panelenelo <panelenelo@gmail.com>
2026-07-19 23:19:50 -0300

    adding routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from model.model import Post
 from random import randrange
@@ -15,7 +15,7 @@ async def root():
 async def getPosts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def postCreatePosts(payload: Post):
     post_dict = payload.model_dump()
     post_dict["id"] = randrange(0, 1000000)
@@ -26,7 +26,13 @@ async def postCreatePosts(payload: Post):
 async def getPostById(id: int):
     return {"post detail": f"Post {id}"}
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def deletePostById(id: int):
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+@app.put("/posts/{id}", status_code=status.HTTP_206_PARTIAL_CONTENT)
+def putPostById(id: int):
+    return Response(status_code=status.HTTP_206_PARTIAL_CONTENT)
 
 
 # if __name__ == "__main__":
