c009cce1230c4143d5b0efad99f1608fcec8810e
panelenelo <panelenelo@gmail.com>
2026-09-16 20:34:44 -0300

    starting installing redis

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -1,9 +1,15 @@
+from typing import Annotated
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError, VerificationError, InvalidHashError
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    pass
 
 def passHashing(password: str) -> str:
     ph = PasswordHasher()
