17a5f1c0d0f63e2f06cbdc0353512c6a4d94472e
panelenelo <panelenelo@gmail.com>
2026-09-23 14:43:12 -0300

    function for creation of both the access and refresh tokens

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Annotated
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError, VerificationError, InvalidHashError
@@ -5,19 +6,19 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
 from os import getenv
-import uuid
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import jwt
+from app.model.model import Token
 
 
-# Create a single global PasswordHasher obj
-# and use it in the functions.
 ph = PasswordHasher()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 load_dotenv()
 jwt_key = getenv("JWT_SECRET_KEY")
 jwt_algo = getenv("JWT_ALGORITHM")
+refresh_exp = getenv("REFRESH_TOKEN_EXPIRE_DAYS")
+access_exp = getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 if (jwt_key is None) or (jwt_algo is None):
     raise RuntimeError("KEY environment variable is not set")
 
@@ -39,9 +40,17 @@ def verifyPass(stored_hash: str, password: str) -> bool:
     except VerificationError:
         raise
 
-def create_token(data: dict, time_delta: timedelta) -> str:
-    to_encode = data.copy()
+def create_token(data: dict, duration: timedelta) -> str:
+    to_encode = {**data}
     now = datetime.now(timezone.utc)
     jti = str(uuid.uuid4())
-    to_encode.update({"iat":now, "exp":now+time_delta, "jti":jti})
+    to_encode.update({"iat":now, "exp":now+duration, "jti":jti})
     return jwt.encode(to_encode, jwt_key, algorithm=jwt_algo)
+
+def create_refresh_token(data: dict) -> str:
+    refresh_token = create_token({**data, "type":"refresh"}, timedelta(days=refresh_exp))
+    return refresh_token
+
+def create_access_token(data: dict) -> str:
+    return create_token({**data, "type":"access"}, timedelta(minutes=access_exp))
+    
