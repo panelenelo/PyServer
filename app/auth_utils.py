fd71fecbf2ad9b3eefc3ca3f5ab6b28685bf5636
panelenelo <panelenelo@gmail.com>
2026-09-22 21:16:43 -0300

    global password hasher object

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -13,7 +13,7 @@ from jose import jwt
 
 # Create a single global PasswordHasher obj
 # and use it in the functions.
-
+ph = PasswordHasher()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 load_dotenv()
 jwt_key = getenv("JWT_SECRET_KEY")
@@ -26,12 +26,10 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     pass
 
 def passHashing(password: str) -> str:
-    ph = PasswordHasher()
     hashed = ph.hash(password)
     return hashed
 
 def verifyPass(stored_hash: str, password: str) -> bool:
-    ph = PasswordHasher()
     try:
         return ph.verify(stored_hash, password)        
     except VerifyMismatchError:
