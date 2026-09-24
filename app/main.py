3f33bb1bae9bde0529df509874c9957f52b33fcb
panelenelo <panelenelo@gmail.com>
2026-09-24 17:32:21 -0300

    successfull connection to Redis, and created depends function

--- app/main.py
+++ app/main.py
@@ -4,12 +4,21 @@ from app.routers.users import router as user_router
 from app.routers.posts import router as post_router
 from app.routers.test  import router as test_router
 from app.database import create_db_and_tables, engine
+from app.redis_utils import create_redis_client, create_redis_pool
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
+    pool = create_redis_pool()
+    client = create_redis_client(pool=pool)
+    app.state.redis_pool = pool
+    app.state.redis = client
+    if client.ping():
+        print("Ping sucessful")
     yield
     engine.dispose()
+    await client.aclose()
+    await pool.aclose()
 
 app = FastAPI(lifespan=lifespan)
 
--- app/redis_utils.py
+++ app/redis_utils.py
@@ -1,2 +1,26 @@
+import os
 import redis.asyncio as aredis
+from fastapi import Request
+from dotenv import load_dotenv
 
+# Load the .env
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_PASS = os.getenv("REDIS_PASS")
+
+def create_redis_pool() -> aredis.BlockingConnectionPool:
+    return aredis.BlockingConnectionPool(
+        host="localhost",
+        port=REDIS_PORT,
+        max_connections=8,  
+        timeout=5,           
+        decode_responses=True,
+    )
+
+def create_redis_client(pool: aredis.BlockingConnectionPool) -> aredis.Redis:
+    return aredis.Redis(connection_pool=pool)
+
+async def get_redis(request: Request) -> aredis.Redis:
+    return request.app.state.redis
